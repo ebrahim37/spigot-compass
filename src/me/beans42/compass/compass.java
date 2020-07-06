e99ebc4c7638f9e1fe62e1b69368de34397d7c69
Ebrahim <53321702+ebrahim37@users.noreply.github.com>
2020-07-06 02:22:01 -0400

    Update compass.java

--- src/me/beans42/compass/compass.java
+++ src/me/beans42/compass/compass.java
@@ -1,52 +1,72 @@
 package me.beans42.compass;
 
 import org.bukkit.Bukkit;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
+import java.util.HashMap;
+
 public class compass extends JavaPlugin implements Listener {
+	static public HashMap<Player, Player> hm = new HashMap<Player, Player>();
+	
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(this, this);
+    	new BukkitRunnable() {
+    		public void run() {
+    	        for (HashMap.Entry<Player, Player> element : hm.entrySet()) {
+    	            final Player tracker = element.getKey();
+    	            final Player target = element.getValue();
+    	      	    
+                    if (!tracker.isOnline() || !target.isOnline()) //if player or target is offline
+                        continue;
+                    else
+                    	tracker.setCompassTarget(target.getLocation());
+    	        }
+    		}
+    	}.runTaskTimer(this, 5L, 20L);
+    }
+	
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        if (args.length != 2) {
-            sender.sendMessage("usage: /compass <tracking player> <tracked player>");
+        if (args.length != 3) {
+            sender.sendMessage("usage: /compass <track/stop> <tracking player> <tracked player (ignored if first arg is stop)>");
         	return true;
         }
     	
-    	final Player player = Bukkit.getPlayerExact(args[0]);
-        final Player target = Bukkit.getPlayerExact(args[1]);
+    	final Player player = Bukkit.getPlayerExact(args[1]);
+        final Player target = Bukkit.getPlayerExact(args[2]);
 
         if (player == null) {
-        	sender.sendMessage(args[0] + " isn't online or is not a valid player");
+        	sender.sendMessage(args[1] + " isn't online or is not a valid player");
         	return true;
         }
         
         if (target == null || !player.canSee(target)) {
-            sender.sendMessage(args[1] + " isn't online or is not a valid player");
+            sender.sendMessage(args[2] + " isn't online or is not a valid player");
             return true;
         }
         
-        player.sendMessage("your compass is now tracking " + target.getName());
+        if (args[0].equals("track")) {
+        	if (hm.containsKey(player))
+        		hm.replace(player, target);
+        	else
+        		hm.put(player, target);
+            player.sendMessage("your compass is now tracking " + target.getName());
+            return true;
+        }
 
-        new BukkitRunnable() {
-            public void run() {
-                if (!player.isOnline()) //if player is offline
-                    this.cancel();
-
-                else if (!target.isOnline()) { //if target is offline
-                    player.sendMessage(target.getName() + " is offline. resetting compass to spawn");
-                    sender.sendMessage(target.getName() + " is offline. resetting compass to spawn");
-                    player.setCompassTarget(player.getWorld().getSpawnLocation());
-                    this.cancel();
-                }
-
-                else
-                    player.setCompassTarget(target.getLocation());
-            }
-        }.runTaskTimer(this, 5L, 100L);
+        if (args[0].equals("stop")) {
+        	hm.remove(player);
+        	player.setCompassTarget(player.getWorld().getSpawnLocation());
+            player.sendMessage("your compass is now pointing to spawn");
+            return true;
+        }
+        
+        sender.sendMessage("first arg needs to be track or stop");
         return true;
     }
 }
